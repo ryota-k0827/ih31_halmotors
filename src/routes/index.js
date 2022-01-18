8a992f39b83b1a8f71fe7081f897d83315190b25
Ryota Kaneko <ryota171124@icloud.com>
2022-01-18 18:17:40 +0900

    add

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.render("./index.ejs");
+const { MySQLClient, sql } = require("../lib/database/client.js");
+
+router.get("/", async (req, res, next) => {
+  let results;
+
+  try {
+    results = await MySQLClient.executeQuery(
+      await sql("SELECT_CAR_LIST")
+    );
+    // console.log(results);
+    res.render("./search/list.ejs", { results });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
